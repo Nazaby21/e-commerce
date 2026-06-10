b9d3aa6c0adcd9c5ea96940ef614dcf7da622c30
Measrithy_Nazaby <nazabymeas7@gmail.com>
2026-06-10 18:39:35 +0700

    close json format 'snake_case'

--- src/main/java/com/school/ecommerce/Vo/Request/UpdateCategoryRequestVo.java
+++ src/main/java/com/school/ecommerce/Vo/Request/UpdateCategoryRequestVo.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 
 public record UpdateCategoryRequestVo(
         @NotBlank(message = "Category name is require")
-        String categoryName,it
+        String categoryName,
         String description
 ) {
 }
